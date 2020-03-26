e1083e1c34f155e0266854ac0597152346d20074
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-03-26 15:27:58 +0100

    ...

--- api/date.go
+++ api/date.go
@@ -8,20 +8,6 @@ import (
 	"os"
 )
 
-func main() {
-	log.Print("log main")
-	fmt.Fprint(os.Stdout, "os.Stdout main")
-	fmt.Fprint(os.Stderr, "os.Stderr main")
-	fmt.Print("fmt.Print main")
-}
-
-func init() {
-	log.Print("log init")
-	fmt.Fprint(os.Stdout, "os.Stdout init")
-	fmt.Fprint(os.Stderr, "os.Stderr init")
-	fmt.Print("fmt.Print init")
-}
-
 func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("log Handler")
 	fmt.Fprint(os.Stdout, "os.Stdout Handler")
