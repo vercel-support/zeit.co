f7a2b815278380cab96d3bc1363ac18bf242b3e8
mbraeutig <m.braeutigam@posteo.de>
2020-03-26 17:23:47 +0100

    ...

--- api/date.go
+++ api/date.go
@@ -1,18 +1,12 @@
 package handler
 
 import (
-	"net/http"
-	"time"	
-	"log"
 	"fmt"
-	"os"
+	"net/http"
+	"time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	log.Print("log Handler")
-	fmt.Fprint(os.Stdout, "os.Stdout Handler")
-	fmt.Fprint(os.Stderr, "os.Stderr Handler")
-	fmt.Print("fmt.Print Handler")
 	currentTime := time.Now().Format(time.RFC850)
 	fmt.Fprintf(w, currentTime)
 }
