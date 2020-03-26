09236de8ecddd08127d6307d562b0fc9a1e44115
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-03-26 12:45:19 +0100

    more logging tests

--- api/date.go
+++ api/date.go
@@ -12,12 +12,14 @@ func init() {
 	log.Print("log init")
 	fmt.Fprint(os.Stdout, "os.Stdout init")
 	fmt.Fprint(os.Stderr, "os.Stderr init")
+	fmt.Print("fmt.Print init")
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("log Handler")
 	fmt.Fprint(os.Stdout, "os.Stdout Handler")
 	fmt.Fprint(os.Stderr, "os.Stderr Handler")
+	fmt.Print("fmt.Print Handler")
 	currentTime := time.Now().Format(time.RFC850)
 	fmt.Fprintf(w, currentTime)
 }
