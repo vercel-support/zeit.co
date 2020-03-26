9d6e3f0448f7c52c200ece963cf8cdc88d43081d
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-03-26 12:25:49 +0100

    testing log and init

--- api/date.go
+++ api/date.go
@@ -1,12 +1,23 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
-	"time"
+	"time"	
+	"log"
+	"fmt"
+	"os"
 )
 
+func init() {
+	log.Print("log init")
+	fmt.Fprint(os.Stdout, "os.Stdout init")
+	fmt.Fprint(os.Stderr, "os.Stderr init")
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("log Handler")
+	fmt.Fprint(os.Stdout, "os.Stdout Handler")
+	fmt.Fprint(os.Stderr, "os.Stderr Handler")
 	currentTime := time.Now().Format(time.RFC850)
 	fmt.Fprintf(w, currentTime)
 }
