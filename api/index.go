57e2f01b7b8eef803f5baf96b37c3243b2af59c5
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-03-31 17:06:33 +0200

    Update index.go

--- api/index.go
+++ api/index.go
@@ -13,5 +13,5 @@ func init() {
 
 // Index ...
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go on Now!</h1>"+str)
+	fmt.Fprintf(w, "<h1>Hello from Go on Now!</h1>"+Str)
 }
