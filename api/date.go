a9d507a3733f75437ff31ea59e9f7c5ca7040da1
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-03-31 11:39:52 +0200

    Update date.go

--- api/date.go
+++ api/date.go
@@ -7,7 +7,7 @@ import (
 )
 
 var initFn string
-var mainFn string
+
 
 func init() {
 	initFn = "init()"
@@ -19,9 +19,4 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	currentTime := time.Now().Format(time.RFC850)
 	fmt.Fprintf(w, currentTime)
 	fmt.Fprintf(w, "\n" + initFn)
-	fmt.Fprintf(w, "\n" + mainFn)
-}
-
-func main() {
-	mainFn = "init()"
 }
